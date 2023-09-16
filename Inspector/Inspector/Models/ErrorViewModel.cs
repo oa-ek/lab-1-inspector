7b537f3c7a7e3d806b2dc25ab72e016d50d28126
Yasya12 <jasja200411@gmail.com>
2023-09-16 21:51:56 +0300

    lab1 - created mvc project

---
+++ Inspector/Inspector/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Inspector.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
