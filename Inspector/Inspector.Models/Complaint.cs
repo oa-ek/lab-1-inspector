36af3ab1d1f2f40063f52d2440ca1a46a15b0356
Yasya12 <jasja200411@gmail.com>
2023-09-17 02:07:37 +0300

    lab1 - added basic entities

---
+++ Inspector/Inspector.Models/Complaint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inspector.Models
+{
+    public class Complaint
+    {
+        public Complaint()
+        {
+            Status = "sent"; 
+            ResponceId = null; 
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } 
+        public string UserName { get; set; }
+
+        //public string UserId { get; set; } реалізувати коли з'явиться Identity
+        [ForeignKey("OrganizationId")]
+        public int OrganizationId { get; set; }
+        public string Description { get; set; } 
+        public string File { get; set; } //img?
+        public string Status { get; set; }
+
+        [ForeignKey("ResponceId")]
+        public int? ResponceId { get; set; }
+        public DateTime CreatedDate { get; set; }    
+    }
+}
